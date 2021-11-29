95e22e48d163bedbe9c00920913061b581bdf7eb
ProsTuha <tuhalf2002@mail.ru>
2021-11-29 14:10:04 +0300

    added header and footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     'class-methods-use-this': 0,
     'no-trailing-spaces': 0,
     'lines-between-class-members': 0,
-				'@typescript-eslint/explicit-module-boundary-types': 'off'
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
   },
   overrides: [
     {
--- src/App.vue
+++ src/App.vue
@@ -25,24 +25,4 @@ export default class App extends Vue {
 </script>
 
 <style lang="scss">
-* {
-  font-family: 'Arial Narrow', sans-serif ;
-}
-h1 {
-    border-bottom: 3px solid #9833CC;
-}
-
-body {
-  height: 100%;
-}
-
-#app {
-  min-height: 100%;
-  width: 100%;
-}
-
-.main_content {
-  height: 100px;
-  margin-bottom: 20px;
-}
 </style>
