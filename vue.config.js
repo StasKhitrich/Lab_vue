ce9eca4abd0be7479727dc0aff38ba9b362ea6b9
ProsTuha <tuhalf2002@mail.ru>
2021-12-02 23:17:37 +0300

    Some fixes

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: '@import "~@/assets/colors.scss";'
+        prependData: '@import "@/assets/colors.scss";'
       }
     }
   }
