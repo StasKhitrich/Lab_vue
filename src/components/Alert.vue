59313b6d2e16d41346f85b85fa38a492912c2b98
ProsTuha <tuhalf2002@mail.ru>
2021-12-07 11:33:46 +0300

    alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="alert" 
+    :class="{'alert-warning': hasWarning, 
+    'alert-info': hasInfo, 
+    'alert-error': hasError, 
+    'alert-success': hasSuccess}" 
+    role="alert">
+        This is a primary alert—check it out!
+    </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+  @Options({
+  })
+
+export default class App extends Vue {
+    hasWarning = false;
+    hasInfo = false;
+    hasError = false;
+    hasSuccess = false;
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
